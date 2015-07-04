fa70a0355bc6aeb7475d7292e7c80bc1b4fbbd6a
Prateek Garg <prateekgarg95@gmail.com>
2015-07-04 21:33:57 +0530

    Initial commit

---
+++ app/src/main/java/com/crapp/testapp/Classroom.java
@@ -0,0 +1,6 @@
+package com.crapp.testapp;
+
+public class Classroom {
+    private String classroomID,classroomName,classroomTeacher;
+    private String[] classroomStudents;
+}
---
+++ app/src/main/java/com/crapp/testapp/Message.java
@@ -0,0 +1,6 @@
+package com.crapp.testapp;
+
+
+public class Message {
+    private String messageText,sender,receiverClassroomName,receiverClassroomID;
+}
